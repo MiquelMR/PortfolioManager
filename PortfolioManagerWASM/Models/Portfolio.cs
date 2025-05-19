5ef51672ff4fe45e9ba1261df6701e4c8d9a0341
MiquelMR <mmauridev@gmail.com>
2025-05-19 10:14:00 +0200

    Name can't be empty validation in portfolio creation

--- PortfolioManagerWASM/Models/Portfolio.cs
+++ PortfolioManagerWASM/Models/Portfolio.cs
@@ -5,6 +5,7 @@ namespace PortfolioManagerWASM.Models
     public class Portfolio
     {
         public int PortfolioId { get; set; }
+        [Required(ErrorMessage = "Name field can't be empty")]
         public string Name { get; set; }
         public string Author { get; set; }
         public string Description { get; set; }
